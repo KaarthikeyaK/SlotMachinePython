a7232f2056e88fe667b6aa73f3da8d80dac2ea8c
KaarthikeyaK <kaarthu2003@gmail.com>
2023-03-23 14:45:38 +0530

    Created a method to bet on single line

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 def deposit():
     while True:
@@ -27,10 +29,26 @@ def getNumberOfLines():
             print("please enter a number")
     return lines
 
+def getBet():
+    while True:
+        amount = input("What would you like to bet on each line? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
+        else:
+            print("Please enter a number.")
+    return amount
+
 
 def main():
     balance = deposit()
     lines = getNumberOfLines()
+    bet = getBet()
+    totalBet = bet * lines
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${totalBet}")
     print(balance, lines)
 
 main()
